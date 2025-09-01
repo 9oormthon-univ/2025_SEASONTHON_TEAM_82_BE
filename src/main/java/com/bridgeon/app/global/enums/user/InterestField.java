52afd18ba42cecd2621289b597d351081d14fbb9
chanition <chanyeong1206@gmail.com>
2025-09-01 16:24:47 +0900

    ✨ Feature: "사용자 관련 enum 작성"
    지역, 관심 분야 enum 작성
    
    related to: #6

--- src/main/java/com/bridgeon/app/global/enums/user/InterestField.java
+++ src/main/java/com/bridgeon/app/global/enums/user/InterestField.java
@@ -1,4 +1,10 @@
 package com.bridgeon.app.global.enums.user;
 
 public enum InterestField {
+    FOOD, // 요식업
+    EDUCATION, // 교육업
+    STARTUP_IT, // 스타트업/IT
+    ECO, // 친환경
+    LOGISTICS, // 유통업
+    SERVICE // 서비스업
 }
