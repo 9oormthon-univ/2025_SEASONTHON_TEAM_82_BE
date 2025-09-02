bd455f8dc5b9ea7e8071a1de558fce1a30536a16
chanition <chanyeong1206@gmail.com>
2025-09-02 16:14:42 +0900

    ✨ feature: "구인게시판 인증 연동"
    유저가 null이면 인증되지 않은 사용자가 뜨도록 수정
    
    related to: #10

--- src/main/java/com/bridgeon/app/domain/board/service/EmployDetailService.java
+++ src/main/java/com/bridgeon/app/domain/board/service/EmployDetailService.java
@@ -4,27 +4,28 @@ package com.bridgeon.app.domain.board.service;
 import com.bridgeon.app.domain.board.dto.response.EmployPostItemResponseDto;
 import com.bridgeon.app.domain.board.entity.EmployBoard;
 import com.bridgeon.app.domain.board.repositroy.EmployBoardJpaRepository;
+import com.bridgeon.app.domain.board.repositroy.EmployScrapJpaRepository;
 import com.bridgeon.app.global.exception.custom.BusinessException;
 import com.bridgeon.app.global.exception.error.EmployErrorCode;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class EmployDetailService {
 
     private final EmployBoardJpaRepository employBoardJpaRepository;
+    private final EmployScrapJpaRepository employScrapJpaRepository;
 
-    public EmployPostItemResponseDto getDetail(Long employBoardId) {
+    public EmployPostItemResponseDto getDetail(Long employBoardId, Long userId) {
         EmployBoard board = employBoardJpaRepository.findByIdAndDeletedAtIsNull(employBoardId)
                 .orElseThrow(() -> new BusinessException(EmployErrorCode.EMPLOY_BOARD_NOT_FOUND));
 
-        // TODO: 인증 붙으면 현재 사용자 스크랩 여부 확인
+
+        boolean isScraped = employScrapJpaRepository.existsByUserIdAndEmployBoardId(userId, board.getId());
+
+
         return EmployPostItemResponseDto.of(board, false);
     }
 }
